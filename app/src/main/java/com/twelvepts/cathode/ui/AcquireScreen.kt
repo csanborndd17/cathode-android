be1ed00e22dfd734df4f6054ae466219bbabe6b4
csanborndd17 <csanborndd17@gmail.com>
2026-09-20 05:15:00 -0500

    Fix Acquire screen dimension import

--- app/src/main/java/com/twelvepts/cathode/ui/AcquireScreen.kt
+++ app/src/main/java/com/twelvepts/cathode/ui/AcquireScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 
 private const val MONOCHROME_URL = "https://monochrome.samidy.com/"
